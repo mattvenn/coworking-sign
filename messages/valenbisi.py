3b33bd0103f76deb663eca0198dd12944b7982d7
matt venn <matt@mattvenn.net>
2018-08-15 19:46:47 +0200

    valenbisi working

--- messages/valenbisi.py
+++ messages/valenbisi.py
@@ -1,24 +1,31 @@
-from messages import is_pi, Message
+from messages import is_pi, TwoLineMessage
 if is_pi():
     import alphasign
 
 import logging
 import requests
+from datetime import datetime
 from xml.etree import ElementTree
 
 log = logging.getLogger(__name__)
 
-class ValenbisiMessage(Message):
+class ValenbisiMessage(TwoLineMessage):
 
     def __init__(self, label, url):
         self.url = url
-        super(Valenbisi, self).__init__(label)
+        super(ValenbisiMessage, self).__init__(label)
         self.update_period = 10 * 60 # 10 minutes in seconds
 
     def do_update(self):
 
-	response = requests.get(self.url)
-	tree = ElementTree.fromstring(response.content)
+        response = requests.get(self.url)
+        tree = ElementTree.fromstring(response.content)
+        available = tree.find('available').text
+        total = tree.find('total').text
+        updated = int(tree.find('updated').text)
+        self.bot = "%s of %s bikes ready" % (available, total)
+        updated_time = datetime.fromtimestamp(updated).strftime("%H:%M:%S")
+        self.top = "Valenbisi last updated: %s" % updated_time
 
     # override get_text so we can force scroll with high speed
     def get_text(self):
